44312bc1139b5ef9e2ad8433f28007dcbc3828d4
Claire Gregg <clairegregg@clairegregg.com>
2025-08-07 12:42:56 +0100

    Seperate surgery editing into component

--- AdminEdit/app/newSurgery.tsx
+++ AdminEdit/app/newSurgery.tsx
@@ -1,37 +1,19 @@
 import { useRouter } from "expo-router";
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { View } from "react-native";
 import {
   PaperProvider,
   Appbar,
-  TextInput,
-  List,
   Button,
-  IconButton,
   useTheme,
 } from 'react-native-paper';
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import {MarkdownTextInput, parseExpensiMark} from '@expensify/react-native-live-markdown';
+import EditSurgery, {consideration} from "@/components/EditSurgeryComponent";
 
-
-type consideration = {
-  title: string,
-  short_title: string,
-  contents: string
-}
 
 const url = 'https://backend.aisling.clairegregg.com/surgery'; 
 
-function emptyConsideration(): consideration {
-  return {
-    title: "",
-    short_title: "",
-    contents: ""
-  }
-}
 
 export default function Surgeries() {
-  const insets = useSafeAreaInsets();
   const router = useRouter()
   const theme = useTheme()
 
@@ -40,44 +22,7 @@ export default function Surgeries() {
   const [type, setType] = React.useState("");
   const [summary, setSummary] = React.useState("");
   const [considerations, setConsiderations] = React.useState<consideration[]>([]);
-  let considerationElements = considerations.map((_, index) =>
-  {
-    return <List.Section>
-      <View style={{flexDirection: "row"}}>
-      <List.Subheader>Consideration {index+1}</List.Subheader>
-      <IconButton icon="trash-can" onPress={ () => {
-        const newConsiderations = considerations.filter((_, indexInArray) => indexInArray !== index);
-        setConsiderations(newConsiderations);
-      }}/>
-      </View>
-      <TextInput label="Title" mode="outlined" value={considerations[index].title} onChangeText={title => {
-        const newConsiderations = [...considerations];
-        newConsiderations[index] = {
-          ...newConsiderations[index],
-          title: title,
-        };  
-        setConsiderations(newConsiderations);
-      }}/>
-      <TextInput label="Short Title" mode="outlined" value={considerations[index].short_title} onChangeText={short_title => {
-        const newConsiderations = [...considerations];
-        newConsiderations[index] = {
-          ...newConsiderations[index],
-          short_title: short_title,
-        };  
-        setConsiderations(newConsiderations);
-      }}/>
-      <MarkdownTextInput value={considerations[index].contents} parser={parseExpensiMark} multiline={true} onChangeText={ contents => {
-        const newConsiderations = [...considerations];
-        newConsiderations[index] = {
-          ...newConsiderations[index],
-          contents: contents,
-        };  
-        setConsiderations(newConsiderations);
-      }} />
-    </List.Section>
-  })
-
-
+  
   return (
     <PaperProvider>
       <View style={{ flex: 1, backgroundColor: theme.colors.surface }}>
@@ -86,25 +31,14 @@ export default function Surgeries() {
             <Appbar.Content title="Surgeries" />
         </Appbar.Header>
       
-        <ScrollView style={{ marginBottom: insets.bottom }}>
-          <TextInput label="Name" mode="outlined" value={name} onChangeText={name => setName(name)}/>
-          <TextInput label="Primary Association" mode="outlined" value={association} onChangeText={association => setAssociation(association)}/>
-          <TextInput label="Type" mode="outlined" value={type} onChangeText={type => setType(type)}/>
-          <MarkdownTextInput value={summary} onChangeText={setSummary} parser={parseExpensiMark} multiline={true}/>
-          <Button onPress={() => {
-            const newConsiderations = [...considerations, emptyConsideration()];
-            setConsiderations(newConsiderations);
-          }}>
-            Add OT Consideration
-          </Button>
-          {considerationElements}
-          <Button onPress={async () => {
-            await addSurgery(name, association, type, summary, considerations)
-            router.back()
-          }}>
-            Add this surgery
-          </Button>
-        </ScrollView>
+        <EditSurgery name={name} setName={setName} association={association} setAssociation={setAssociation} 
+          type={type} setType={setType} summary={summary} setSummary={setSummary} considerations={considerations} setConsiderations={setConsiderations}/>
+        <Button onPress={async () => {
+          await addSurgery(name, association, type, summary, considerations)
+          router.back()
+        }}>
+          Add this surgery
+        </Button>
       </View>
     </PaperProvider>
   );
---
+++ AdminEdit/components/EditSurgeryComponent.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { ScrollView, View } from "react-native";
+import {
+  TextInput,
+  List,
+  Button,
+  IconButton,
+} from 'react-native-paper';
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+// import {MarkdownTextInput, parseExpensiMark} from '@expensify/react-native-live-markdown';
+
+
+export type consideration = {
+  title: string,
+  short_title: string,
+  contents: string
+}
+
+function emptyConsideration(): consideration {
+  return {
+    title: "",
+    short_title: "",
+    contents: ""
+  }
+}
+
+interface EditSurgeryProps {
+    name: string,
+    setName: React.Dispatch<React.SetStateAction<string>>,
+    association: string,
+    setAssociation: React.Dispatch<React.SetStateAction<string>>,
+    type: string,
+    setType: React.Dispatch<React.SetStateAction<string>>,
+    summary: string,
+    setSummary: React.Dispatch<React.SetStateAction<string>>,
+    considerations: consideration[],
+    setConsiderations: React.Dispatch<React.SetStateAction<consideration[]>>
+}
+
+export default function EditSurgery({name, setName, association, setAssociation, type, setType, summary, setSummary, considerations, setConsiderations}: EditSurgeryProps) {
+  const insets = useSafeAreaInsets();
+
+  let considerationElements = considerations.map((_, index) =>
+  {
+    return <List.Section>
+      <View style={{flexDirection: "row"}}>
+      <List.Subheader>Consideration {index+1}</List.Subheader>
+      <IconButton icon="trash-can" onPress={ () => {
+        const newConsiderations = considerations.filter((_, indexInArray) => indexInArray !== index);
+        setConsiderations(newConsiderations);
+      }}/>
+      </View>
+      <TextInput label="Title" mode="outlined" value={considerations[index].title} onChangeText={title => {
+        const newConsiderations = [...considerations];
+        newConsiderations[index] = {
+          ...newConsiderations[index],
+          title: title,
+        };  
+        setConsiderations(newConsiderations);
+      }}/>
+      <TextInput label="Short Title" mode="outlined" value={considerations[index].short_title} onChangeText={short_title => {
+        const newConsiderations = [...considerations];
+        newConsiderations[index] = {
+          ...newConsiderations[index],
+          short_title: short_title,
+        };  
+        setConsiderations(newConsiderations);
+      }}/>
+      {/* <MarkdownTextInput value={considerations[index].contents} parser={parseExpensiMark} multiline={true} onChangeText={ contents => {
+        const newConsiderations = [...considerations];
+        newConsiderations[index] = {
+          ...newConsiderations[index],
+          contents: contents,
+        };  
+        setConsiderations(newConsiderations);
+      }} /> */}
+    </List.Section>
+  })
+
+
+  return (
+    <ScrollView style={{ marginBottom: insets.bottom }}>
+        <TextInput label="Name" mode="outlined" value={name} onChangeText={name => setName(name)}/>
+        <TextInput label="Primary Association" mode="outlined" value={association} onChangeText={association => setAssociation(association)}/>
+        <TextInput label="Type" mode="outlined" value={type} onChangeText={type => setType(type)}/>
+        {/* <MarkdownTextInput value={summary} onChangeText={setSummary} parser={parseExpensiMark} multiline={true}/> */}
+        <Button onPress={() => {
+        const newConsiderations = [...considerations, emptyConsideration()];
+        setConsiderations(newConsiderations);
+        }}>
+        Add OT Consideration
+        </Button>
+        {considerationElements}
+    </ScrollView>
+  );
+}
+
+
